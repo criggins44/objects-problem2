c20ed2aa95cb5df601229d23d03a5f4dd2953325
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-23 11:56:03 -0400

    created the test object

---
+++ scirpt.js
@@ -0,0 +1,25 @@
+//Object Problem 2
+
+/*
+Create an array of people objects with first name, last name, and age, 
+then write a JavaScript function that takes in an array and console logs 
+the first and last names of all the people.
+*/
+
+let people = {
+
+	'Clark':{
+		'last': 'Kent',
+		'age': 35,
+	},
+	
+	'Peter':{
+		'last': 'Parker',
+		'age': 16,
+	},
+
+	'Bruce': {
+		'last': 'Wayne',
+		'age': 35,
+	},
+}
